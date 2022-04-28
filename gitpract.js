dea2e880cdb8daacbbc14283f127d739ce493f18
Lyss2120 <lys.ivonne2020@gmail.com>
2022-04-28 16:54:31 -0400

    mi primer commit

---
+++ gitpract.js
@@ -0,0 +1,11 @@
+var age = prompt("What's your age?");
+
+if (age.toLowerCase() == "34") {
+  alert("Wow! I'm that age too!");
+}
+if (age.toLowerCase() >= "50") {
+    alert("Wow! you have to start to take care of your health!");
+  }
+  else{
+      alert("Wow! what a great age!");
+}
